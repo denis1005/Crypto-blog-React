965a7b32e1096923b3d2195d89071006bb9d8681
Denislav <denis1005@users.noreply.github.com>
2022-08-14 23:30:20 +0300

    Adding login and logout pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import {Route,Routes } from 'react-router-dom'
 import {NavbarComponent} from './Components/Navbar'
 import {Footer} from './Components/Footer'
 import {Home} from './Components/Home/Home'
-
+import { Login } from './Components/Login/Login';
+import { Register } from './Components/Register/Register';
 
 import './App.css';
+
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
     <NavbarComponent/>
      <Routes>
       <Route path="/" element={ <Home/>}/>
+      <Route path="/login" element={ <Login/>}/>
+      <Route path="/register" element={ <Register/>}/>
      </Routes>
      
     <Footer/>
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-
+import {Link} from 'react-router-dom'
 export const NavbarComponent=()=> {
     return (
         <nav className="navbar navbar-expand-lg navbar-red navbar-dark">
@@ -6,33 +6,33 @@ export const NavbarComponent=()=> {
           
         </div>
   <div className="container-fluid all-show">
-    <a className="navbar-brand" href="#/">Penton <i className="fa fa-codepen"></i></a>
+    <Link className="navbar-brand" to="/">Penton <i className="fa fa-codepen"></i></Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#/">About us</a>
+          <Link className="nav-link active" aria-current="page" to="#/">About us</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#/">Memes</a>
-        </li>
-
-        <li className="nav-item">
-          <a className="nav-link" href="#/">Login</a>
+          <Link className="nav-link" to="#/">Memes</Link>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="#/">Logout</a>
+          <Link className="nav-link" to="/login">Login</Link>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="#/">Register</a>
+          <Link className="nav-link" to="#/">Logout</Link>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="#/"><i className="fa fa-search"></i></a>
+          <Link className="nav-link" to="/register">Register</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link" to="#/"><i className="fa fa-search"></i></Link>
         </li>
         
         
